65d4a0781fa7da52b4a4f7b75933fc04dd17bb88
AblamDev <ablamamenoussi9@gmail.com>
2024-06-25 15:59:43 +0000

    Setup and authentification

---
+++ app/Http/Controllers/SaleItemController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sale_item;
+use App\Http\Requests\StoreSale_itemRequest;
+use App\Http\Requests\UpdateSale_itemRequest;
+
+class SaleItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSale_itemRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sale_item $sale_item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sale_item $sale_item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSale_itemRequest $request, Sale_item $sale_item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sale_item $sale_item)
+    {
+        //
+    }
+}
